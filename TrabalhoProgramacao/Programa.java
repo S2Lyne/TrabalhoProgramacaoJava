55eb50720c806f30f4af76463cbc0e0a82b66ac0
Aline Athaydes <54457125+S2Lyne@users.noreply.github.com>
2023-05-30 15:02:27 -0300

    Update Programa.java
    Alterando a verificação e encerrando o programa caso digite 0

--- TrabalhoProgramacao/Programa.java
+++ TrabalhoProgramacao/Programa.java
@@ -8,30 +8,33 @@ public class Programa {
 		
 		//CRIANDO UM OBJETOS DA CLASSE SCANNER
 		Scanner scanner = new Scanner(System.in);
-        int opcao;
+        int opcao= -1;
         
        
         Cliente.saveCliente();
         Servico.saveServico();
         Funcionario.saveFuncionario();
         
-        //CHAMADA DO MENU INICIAL
-        System.out.println("*********** SALÃO DAS DONZELAS DE TI *********** ");
-        System.out.println("**************** MENU PRINCIPAL **************** ");
-        System.out.println("************* Selecione uma opção: ************* ");
-        System.out.println("**************** 1 - Cadastrar ***************** ");
-        System.out.println("**************** 2 - Atualizar ***************** ");
-        System.out.println("**************** 3 - Deletar ******************* ");
-        System.out.println("**************** 4 - Listar ******************** ");
-        //System.out.println("**************** 4 - Atendimento *************** ");
-        System.out.println("**************** 0 - SAIR ********************** ");
-        
-        opcao = scanner.nextInt();
-        Global.menu(opcao);
-        
-     
-
-        scanner.close();
+        if(opcao == 0) {
+			System.out.println("Saindo...");
+			System.exit(0);
+		}else {
+			//CHAMADA DO MENU INICIAL
+	        System.out.println("*********** SALÃO DAS DONZELAS DE TI *********** ");
+	        System.out.println("**************** MENU PRINCIPAL **************** ");
+	        System.out.println("************* Selecione uma opção: ************* ");
+	        System.out.println("**************** 1 - Cadastrar ***************** ");
+	        System.out.println("**************** 2 - Atualizar ***************** ");
+	        System.out.println("**************** 3 - Deletar ******************* ");
+	        System.out.println("**************** 4 - Listar ******************** ");
+	        //System.out.println("**************** 4 - Atendimento *************** ");
+	        System.out.println("**************** 0 - SAIR ********************** ");
+	        
+	        opcao = scanner.nextInt();
+	        Global.menu(opcao);
+	        
+		}
+       
     }
 	
 	
