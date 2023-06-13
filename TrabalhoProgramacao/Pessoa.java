5932b4afdc0d04d728d8a513cb3285a1ef43a24a
Aline Athaydes <54457125+S2Lyne@users.noreply.github.com>
2023-06-12 23:04:16 -0300

    Update Pessoa.java
    Atualizando inserindo construtor sem parametro

--- TrabalhoProgramacao/Pessoa.java
+++ TrabalhoProgramacao/Pessoa.java
@@ -1,47 +1,51 @@
 package TrabalhoProgramacao;
+
 public class Pessoa {
-    
-   	private String nome;
-    	private String dataNascimento;
-    	private String telefone;
-    	private String email;
-    
-    	// Constructor	
+
+	private String nome;
+	private String dataNascimento;
+	private String telefone;
+	private String email;
+
+	// Constructor	
 	public Pessoa (String nome, String dataNascimento, String telefone, String email) {
-        	this.nome = nome;
-        	this.dataNascimento = dataNascimento;
-        	this.telefone = telefone;
-        	this.email = email;
-    	}
-    
-    	public String getNome() {
-		return nome;
+    	this.nome = nome;
+    	this.dataNascimento = dataNascimento;
+    	this.telefone = telefone;
+    	this.email = email;
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+	public Pessoa() {
+		
 	}
 
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
+	public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
 
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
+public String getDataNascimento() {
+	return dataNascimento;
+}
 
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
+public void setDataNascimento(String dataNascimento) {
+	this.dataNascimento = dataNascimento;
+}
+public String getTelefone() {
+	return telefone;
+}
 
-	public String getEmail() {
-		return email;
-	}
+public void setTelefone(String telefone) {
+	this.telefone = telefone;
+}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-    
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
 }
